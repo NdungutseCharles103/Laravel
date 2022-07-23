bc73ea2554546bd551475c6f691e770298719437
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-07-23 16:33:51 +0200

    loginuser

--- react-crud/resources/js/Routes/Login.jsx
+++ react-crud/resources/js/Routes/Login.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+    return (
+        <div className="row justify-content-center pt-5">
+            <div className="col-sm-6">
+                <div className="card p-4">
+                    <div className="form-group">
+                        <label htmlFor="">Email address:</label>
+                        <input type="email" className="form-control"
+                        placeholder="Enter email" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="">Password:</label>
+                        <input type="text" className="form-control"
+                        placeholder="Enter Password" />
+                    </div>
+                    <button type="submit" className="btn btn-primary mt-4">Login</button>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Login
+export default Login;
